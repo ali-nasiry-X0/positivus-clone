2af8c40203176d4c0d827e8382c73feaeabe2333
ali-nasiry-X0 <ali.nasiry.co@gmail.com>
2026-07-28 23:09:12 +0330

    WIP: working on optimization animation and blure navbar and floating menu of nav on small screen

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,7 @@
-const btn_menu = document.querySelector('.navbar__toggle');
-btn_menu.addEventListener("click",()=>{
-    btn_menu.classList.toggle("active")
-})
+const btn_menu = document.querySelector(".navbar__toggle");
+const popup_menu = document.querySelector(".menu-popup");
+btn_menu.addEventListener("click", () => {
+  btn_menu.classList.toggle("active");
+  popup_menu.classList.toggle("open");
+  document.body.classList.toggle("overflow-hidden");
+});
